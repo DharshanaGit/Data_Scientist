804aece0c67b6b55cfebaffe5abc26ccf21217df
DharshanaGit <dharshanab.bit24@rathinam.in>
2025-12-18 13:17:45 +0530

    file added

---
+++ KNN-Project/knnapp.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+# -----------------------------
+# Load saved model & scaler
+# -----------------------------
+model=jb.load("/home/intellect/Documents/Data_Scientist/KNN-Project/Social_network-ads.pkl")
+scaler=jb.load("/home/intellect/Documents/Data_Scientist/KNN-Project/scaler.pkl")
+
+# -----------------------------
+# Title
+# -----------------------------
+st.title("📱 Social Network Ads - KNN Prediction")
+st.write("Predict whether a user will purchase or not")
+
+# -----------------------------
+# User Input
+# -----------------------------
+age = st.number_input("Age", min_value=18, max_value=100, value=30)
+salary = st.number_input("Estimated Salary", min_value=10000, max_value=200000, value=87000)
+
+# -----------------------------
+# Prediction
+# -----------------------------
+if st.button("Predict"):
+    new_data = np.array([[age, salary]])
+    new_data_scaled = scaler.transform(new_data)
+    prediction = knn.predict(new_data_scaled)
+
+    if prediction[0] == 1:
+        st.success("✅ User will PURCHASE")
+    else:
+        st.error("❌ User will NOT purchase")
